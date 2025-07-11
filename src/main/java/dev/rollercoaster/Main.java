eca0aad9dcf96adc7d5ba68524280b35aaf471ac
KirioLerny <nozd-0a2f@stud.hhs.karlsruhe.de>
2025-07-11 23:57:52 +0200

    Fixed command registering

--- src/main/java/dev/rollercoaster/Main.java
+++ src/main/java/dev/rollercoaster/Main.java
@@ -3,6 +3,8 @@ package dev.rollercoaster;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
@@ -10,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         String token = dotenv.get("DISCORD_TOKEN");
 
         JDA jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_VOICE_STATES)
@@ -19,5 +21,13 @@ public class Main {
                 .build();
 
         jda.awaitReady();
+
+        jda.updateCommands()
+                .addCommands(
+                        Commands.slash("rollercoaster", "Start a ride.")
+                                .addOption(OptionType.USER, "target", "Target user", true),
+                        Commands.slash("rc", "Start a ride.")
+                                .addOption(OptionType.USER, "target", "Target user", true)
+                ).queue();
     }
 }
